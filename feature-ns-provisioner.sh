10dca65fe21a69087063cd3d93f7dc362e291863
Jason Waldrip <jason@waldrip.net>
2017-07-07 10:35:36 -0600

    Create feature-ns-provisioner.sh

---
+++ feature-ns-provisioner.sh
@@ -0,0 +1,9 @@
+set -e
+
+# Install the provisioning script
+export FEATURE_NAMESPACES_RELEASES_URL=https://api.github.com/repos/commercialtribe/kubes-namespace-provisioner/releases/latest?access_token=$GITHUB_API_TOKEN
+export FEATURE_NAMESPACES_DOWNLOAD_URL=`curl -sSL $FEATURE_NAMESPACES_RELEASES_URL | jq -r '.assets[] | select(.name | contains("linux")).url'`
+export ACCEPTED_HEADER=Accept:application/octet-stream
+curl -L -H $ACCEPTED_HEADER -o kubes-namespace-provisioner $FEATURE_NAMESPACES_DOWNLOAD_URL?access_token=$GITHUB_API_TOKEN
+chmod +x ./kubes-namespace-provisioner
+sudo mv ./kubes-namespace-provisioner /usr/local/bin/kubes-namespace-provisioner
