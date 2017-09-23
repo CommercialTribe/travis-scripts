9b5c0ad51326100276a2ab5e1c02ab616071e374
Jason Waldrip <jason@waldrip.net>
2017-09-23 08:16:05 -0600

    Update psykube.sh

--- psykube.sh
+++ psykube.sh
@@ -6,9 +6,7 @@ export PSYKUBE_DOWNLOAD_URL=`curl -sSL $PSYKUBE_RELEASES_URL | jq -r '.assets[] 
 curl -sSL $PSYKUBE_DOWNLOAD_URL | sudo tar -xzC /usr/local/bin
 
 # Install Kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+curl -sSL https://raw.githubusercontent.com/CommercialTribe/travis-scripts/master/kubectl.sh | bash
 
 # Authorize Gcloud
 curl -sSL https://raw.githubusercontent.com/CommercialTribe/travis-scripts/master/gcloud.sh | bash
