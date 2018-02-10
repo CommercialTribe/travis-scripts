2a7d32d53c3926c776d2cf94a66dccd5807c3842
Jason Waldrip <jason@waldrip.net>
2018-02-09 21:59:32 -0700

    Update gcloud.sh

--- gcloud.sh
+++ gcloud.sh
@@ -1,18 +1,21 @@
 set -e
 
 # Re-install Gcloud from Scratch
+echo "Installing gcloud"
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; fi
-pwd
 curl -o google-cloud-sdk-187.0.0-linux-x86_64.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-187.0.0-linux-x86_64.tar.gz
 tar -xzf google-cloud-sdk-187.0.0-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh
 gcloud version
 
 # Authenticate with Google Cloud
+echo "Decoding creds"
 echo $GCLOUD_ENCODED_CREDS | base64 -d > /tmp/gcloud.json
+echo "Activating service account"
 gcloud auth activate-service-account --key-file=/tmp/gcloud.json
 
 # Setup credentials for Google Cloud staging and production
+echo "Fetching cluster config"
 gcloud container clusters get-credentials staging --zone=us-central1-a --project=commercial-tribe-staging
 gcloud container clusters get-credentials production --zone=us-east1-c --project=commercial-tribe
 
