8949f7ba31c4f82b91116af8450fcb0c3bec42cd
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2018-02-28 13:33:05 -0700

    Use latest gcloud

--- gcloud.sh
+++ gcloud.sh
@@ -1,16 +1,21 @@
 set -e
 
+# Uncomment and use if specific gcloud version is needed.
+# GCLOUD_SDK_VERSION=187.0.0
+# export CLOUDSDK_CORE_DISABLE_PROMPTS=1;
+# curl -o ${HOME}/google-cloud-sdk-${GCLOUD_SDK_VERSION}-linux-x86_64.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${GCLOUD_SDK_VERSION}-linux-x86_64.tar.gz
+# tar -xzf ${HOME}/google-cloud-sdk-${GCLOUD_SDK_VERSION}-linux-x86_64.tar.gz
+# ${HOME}/google-cloud-sdk/install.sh -q
+
 # Re-install Gcloud from Scratch
 echo "Installing gcloud"
-if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; fi
-curl -o $HOME/google-cloud-sdk-187.0.0-linux-x86_64.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-187.0.0-linux-x86_64.tar.gz
-tar -xzf $HOME/google-cloud-sdk-187.0.0-linux-x86_64.tar.gz
-$HOME/google-cloud-sdk/install.sh -q
+if [ ! -d "${HOME}/google-cloud-sdk/bin" ]; then rm -rf ${HOME}/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
+source /home/travis/google-cloud-sdk/path.bash.inc
 gcloud version
 
 # Authenticate with Google Cloud
 echo "Decoding creds"
-echo $GCLOUD_ENCODED_CREDS | base64 -d > /tmp/gcloud.json
+echo ${GCLOUD_ENCODED_CREDS} | base64 -d > /tmp/gcloud.json
 echo "Activating service account"
 gcloud auth activate-service-account --key-file=/tmp/gcloud.json
 
