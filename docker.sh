ee88d3a4c5fb601525dde8155023a561b6636204
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2018-02-28 10:24:51 -0700

    Update docker as part of psykube install.

---
+++ docker.sh
@@ -0,0 +1,5 @@
+set -e
+
+# Install latest Docker
+sudo apt-get update
+sudo apt-get install docker-ce
--- psykube.sh
+++ psykube.sh
@@ -2,6 +2,9 @@ set -e
 
 PSYKUBE_VERSION=${PSYKUBE_VERSION-1.9.3.0}
 
+# Update Docker
+curl -sSL https://raw.githubusercontent.com/CommercialTribe/travis-scripts/master/docker.sh | bash
+
 # Install Psykube
 export PSYKUBE_RELEASES_URL=https://api.github.com/repos/commercialtribe/psykube/releases/$PSYKUBE_VERSION
 export PSYKUBE_DOWNLOAD_URL=`curl -sSL $PSYKUBE_RELEASES_URL | jq -r '.assets[] | select(.name | contains("linux")).browser_download_url'`
