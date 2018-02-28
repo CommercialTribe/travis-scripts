662c6f143cb7c47192d231e60d3cd9bb18eb1d5a
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2018-02-28 11:42:54 -0700

    Fix psykube version download, add back docker

--- psykube.sh
+++ psykube.sh
@@ -1,11 +1,14 @@
 set -e
 
-PSYKUBE_VERSION=${PSYKUBE_VERSION-1.9.3.0}
+PSYKUBE_VERSION=v1.9.3.0
+
+# Install latest docker
+curl -sSL https://raw.githubusercontent.com/CommercialTribe/travis-scripts/master/docker.sh | bash
 
 # Install Psykube
-export PSYKUBE_RELEASES_URL=https://api.github.com/repos/commercialtribe/psykube/releases/$PSYKUBE_VERSION
-export PSYKUBE_DOWNLOAD_URL=`curl -sSL $PSYKUBE_RELEASES_URL | jq -r '.assets[] | select(.name | contains("linux")).browser_download_url'`
-curl -sSL $PSYKUBE_DOWNLOAD_URL | sudo tar -xzC /usr/local/bin
+export PSYKUBE_RELEASES_URL=https://api.github.com/repos/psykube/psykube/releases/tags/${PSYKUBE_VERSION}
+export PSYKUBE_DOWNLOAD_URL=`curl -sSL ${PSYKUBE_RELEASES_URL} | jq -r '.assets[] | select(.name | contains("linux")).browser_download_url'`
+curl -sSL ${PSYKUBE_DOWNLOAD_URL} | sudo tar -xzC /usr/local/bin
 
 # Install Kubectl
 curl -sSL https://raw.githubusercontent.com/CommercialTribe/travis-scripts/master/kubectl.sh | bash
