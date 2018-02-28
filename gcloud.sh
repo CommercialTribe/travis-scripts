7e0ebfca8d10fff6208ed37a6444fb791d23957e
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2018-02-28 14:05:12 -0700

    Pipe gcloud install output to dev null

--- gcloud.sh
+++ gcloud.sh
@@ -9,7 +9,7 @@ set -e
 
 # Re-install Gcloud from Scratch
 echo "Installing gcloud"
-if [ ! -d "${HOME}/google-cloud-sdk/bin" ]; then rm -rf ${HOME}/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
+if [ ! -d "${HOME}/google-cloud-sdk/bin" ]; then rm -rf ${HOME}/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash > /dev/null; fi
 source /home/travis/google-cloud-sdk/path.bash.inc
 gcloud version
 
