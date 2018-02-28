5acc4388f0a0917e764f711836e50b0ec1ed726e
Jason Waldrip <jason@waldrip.net>
2018-02-28 09:59:46 -0700

    Update psykube.sh

--- psykube.sh
+++ psykube.sh
@@ -1,7 +1,9 @@
 set -e
 
+PSYKUBE_VERSION=${PSYKUBE_VERSION-1.9.3.0}
+
 # Install Psykube
-export PSYKUBE_RELEASES_URL=https://api.github.com/repos/commercialtribe/psykube/releases/latest
+export PSYKUBE_RELEASES_URL=https://api.github.com/repos/commercialtribe/psykube/releases/$PSYKUBE_VERSION
 export PSYKUBE_DOWNLOAD_URL=`curl -sSL $PSYKUBE_RELEASES_URL | jq -r '.assets[] | select(.name | contains("linux")).browser_download_url'`
 curl -sSL $PSYKUBE_DOWNLOAD_URL | sudo tar -xzC /usr/local/bin
 
