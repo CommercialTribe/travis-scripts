4ce1a471d0361f92979ded012763e1f36aea6fd6
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2018-08-09 03:15:52 -0600

    Update curl

--- gcloud.sh
+++ gcloud.sh
@@ -1,6 +1,6 @@
 set -e
 
-echo "Installing latest gcloud and docker"
+echo "Installing latest docker and gcloud"
 # Create environment variable for correct distribution
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 # Add the Cloud SDK distribution URI as a package source
@@ -9,7 +9,7 @@ echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 # Update the package list and install the Cloud SDK
 sudo apt-get update
-sudo apt-get install google-cloud-sdk docker-ce
+sudo apt-get install curl docker-ce google-cloud-sdk
 
 # Install latest docker-compose
 DOCKER_COMPOSE_VERSION=curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | jq -r .tag_name
