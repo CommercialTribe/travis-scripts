847658c56eacb7269f7d45f5cfc113d798fbe230
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2018-08-09 11:38:59 -0600

    Add log to psykube install and version

--- psykube.sh
+++ psykube.sh
@@ -5,6 +5,10 @@ curl -sSL https://raw.githubusercontent.com/CommercialTribe/travis-scripts/maste
 
 PSYKUBE_VERSION=v1.9.3.0
 
+echo ""
+echo "Installing psykube ${PSYKUBE_VERSION}"
+echo ""
+
 # Install Psykube
 PSYKUBE_RELEASES_URL=https://api.github.com/repos/psykube/psykube/releases/tags/${PSYKUBE_VERSION}
 PSYKUBE_RELEASE_RESULTS=`curl -sSL -H "Authorization: token ${GITHUB_API_TOKEN}" ${PSYKUBE_RELEASES_URL}`
