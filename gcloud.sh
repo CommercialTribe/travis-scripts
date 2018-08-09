481b63bfd2d152ad13c797aec8a3056fc4e50407
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2018-08-09 02:47:13 -0600

    Update gcloud debian package

--- gcloud.sh
+++ gcloud.sh
@@ -1,16 +1,15 @@
 set -e
 
-# Uncomment and use if specific gcloud version is needed.
-# GCLOUD_SDK_VERSION=187.0.0
-# export CLOUDSDK_CORE_DISABLE_PROMPTS=1;
-# curl -o ${HOME}/google-cloud-sdk-${GCLOUD_SDK_VERSION}-linux-x86_64.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${GCLOUD_SDK_VERSION}-linux-x86_64.tar.gz
-# tar -xzf ${HOME}/google-cloud-sdk-${GCLOUD_SDK_VERSION}-linux-x86_64.tar.gz
-# ${HOME}/google-cloud-sdk/install.sh -q
 
-# Re-install Gcloud from Scratch
 echo "Installing gcloud"
-if [ ! -d "${HOME}/google-cloud-sdk/bin" ]; then rm -rf ${HOME}/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash > /dev/null; fi
-source /home/travis/google-cloud-sdk/path.bash.inc
+# Create environment variable for correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
 gcloud version
 
 # Authenticate with Google Cloud
