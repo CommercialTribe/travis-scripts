378d7183cbea5fdf3da6fbe66568b6ab1c60260b
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2018-08-09 10:15:16 -0600

    Make travis script more better.

--- gcloud.sh
+++ gcloud.sh
@@ -1,43 +1,63 @@
 set -e
 
-# TODO Open issue on docker-gcr-cred repo about WARNING! Your password will be stored unencrypted in .../.docker/config.json
-# TODO See if we can install kubectl via apt-get instead of direct download
-# TODO See if we can install docker-compose via apt-get instead of direct download
+echo "################################"
+echo "# Installing latest:           #"
+echo "#  - docker-ce (docker)        #"
+echo "#  - kubectl                   #"
+echo "#  - google-cloud-sdk (gcloud) #"
+echo "################################"
 
-echo "Installing latest docker and gcloud"
-# Create environment variable for correct distribution
+# Setup docker-ce repo
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Setup google-cloud-sdk repo
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-# Add the Cloud SDK distribution URI as a package source
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-# Import the Google Cloud Platform public key
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# Setup kubectl repo
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo touch /etc/apt/sources.list.d/kubernetes.list
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+
 # Update the package list and install the Cloud SDK
 sudo apt-get update
-sudo apt-get install docker-ce google-cloud-sdk
+sudo apt-get install docker-ce google-cloud-sdk kubectl
 
 # Install latest docker-compose
-DOCKER_COMPOSE_RELEASE_URL=https://api.github.com/repos/docker/compose/releases/latest
-DOCKER_COMPOSE_VERSION=`curl -sSL -H "Authorization: token ${GITHUB_API_TOKEN}" ${DOCKER_COMPOSE_RELEASE_URL} | jq -r .tag_name`
-sudo curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# DOCKER_COMPOSE_RELEASE_URL=https://api.github.com/repos/docker/compose/releases/latest
+# DOCKER_COMPOSE_VERSION=`curl -sSL -H "Authorization: token ${GITHUB_API_TOKEN}" ${DOCKER_COMPOSE_RELEASE_URL} | jq -r .tag_name`
+# sudo curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
 
 # Print version information
-echo "docker version"
+echo ""
+echo "##########"
+echo "# docker #"
+echo "##########"
 docker version
-echo "docker-compose version"
+echo "##################"
+echo "# docker-compose #"
+echo "##################"
 docker-compose version
-# TODO kubectl version
-echo "gcloud version"
+echo "###########"
+echo "# kubectl #"
+echo "###########"
+kubectl version
+echo "##########"
+echo "# gcloud #"
+echo "##########"
 gcloud version
+echo ""
+echo "#################################"
+echo "# Authorizing with Google Cloud #"
+echo "#################################"
+echo ""
 
-# Authenticate with Google Cloud
-echo "Decoding creds"
 echo ${GCLOUD_ENCODED_CREDS} | base64 -d > /tmp/gcloud.json
-echo "Activating service account"
 gcloud auth activate-service-account --key-file=/tmp/gcloud.json
 
 # Setup credentials for Google Cloud staging and production
-echo "Fetching cluster config"
 gcloud container clusters get-credentials staging --zone=us-central1-a --project=commercial-tribe-staging
 gcloud container clusters get-credentials production --zone=us-east1-c --project=commercial-tribe
 
--- psykube.sh
+++ psykube.sh
@@ -1,5 +1,8 @@
 set -e
 
+# Install other docker, k8s deps and authorize on GCP
+curl -sSL https://raw.githubusercontent.com/CommercialTribe/travis-scripts/travis-test/gcloud.sh | bash
+
 PSYKUBE_VERSION=v1.9.3.0
 
 # Install Psykube
@@ -7,9 +10,3 @@ PSYKUBE_RELEASES_URL=https://api.github.com/repos/psykube/psykube/releases/tags/
 PSYKUBE_RELEASE_RESULTS=`curl -sSL -H "Authorization: token ${GITHUB_API_TOKEN}" ${PSYKUBE_RELEASES_URL}`
 PSYKUBE_DOWNLOAD_URL=`echo $PSYKUBE_RELEASE_RESULTS | jq -r '.assets[] | select(.name | contains("linux")).browser_download_url'`
 curl -sSL ${PSYKUBE_DOWNLOAD_URL} | sudo tar -xzC /usr/local/bin
-
-# Install Kubectl
-curl -sSL https://raw.githubusercontent.com/CommercialTribe/travis-scripts/travis-test/kubectl.sh | bash
-
-# Authorize Gcloud
-curl -sSL https://raw.githubusercontent.com/CommercialTribe/travis-scripts/travis-test/gcloud.sh | bash
