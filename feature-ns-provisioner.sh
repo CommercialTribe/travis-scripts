0cc560b304669b1679adb88f0fe09b3b0a7a4fcd
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2018-08-27 12:02:18 -0600

    Pin namespace version

--- feature-ns-provisioner.sh
+++ feature-ns-provisioner.sh
@@ -3,10 +3,19 @@ set -e
 # Install Psykube
 curl -sSL https://raw.githubusercontent.com/CommercialTribe/travis-scripts/master/psykube.sh | bash
 
+NAMESPACE_PROVISIONER_VERSION=v1.1.3
+
+echo ""
+echo "Installing namespace provisioner ${NAMESPACE_PROVISIONER_VERSION}"
+echo ""
+
 # Install the provisioning script
-export FEATURE_NAMESPACES_RELEASES_URL=https://api.github.com/repos/commercialtribe/kubes-namespace-provisioner/releases/latest?access_token=$GITHUB_API_TOKEN
-export FEATURE_NAMESPACES_DOWNLOAD_URL=`curl -sSL $FEATURE_NAMESPACES_RELEASES_URL | jq -r '.assets[] | select(.name | contains("linux")).url'`
-export ACCEPTED_HEADER=Accept:application/octet-stream
-curl -L -H $ACCEPTED_HEADER -o kubes-namespace-provisioner $FEATURE_NAMESPACES_DOWNLOAD_URL?access_token=$GITHUB_API_TOKEN
+AUTHORIZATION_HEADER=Authorization: token ${GITHUB_API_TOKEN}
+ACCEPTED_HEADER=Accept: application/octet-stream
+
+NAMESPACE_PROVISIONER_RELEASE_URL=https://api.github.com/repos/commercialtribe/kubes-namespace-provisioner/releases/tags/${NAMESPACE_PROVISIONER_VERSION}
+NAMESPACE_PROVISIONER_DOWNLOAD_URL=`curl -sSL -H ${AUTHORIZATION_HEADER} ${NAMESPACE_PROVISIONER_RELEASE_URL} | jq -r '.assets[] | select(.name | contains("linux")).url'`
+
+curl -sSL -H ${AUTHORIZATION_HEADER} -H ${ACCEPTED_HEADER} -o kubes-namespace-provisioner ${NAMESPACE_PROVISIONER_DOWNLOAD_URL}
 chmod +x ./kubes-namespace-provisioner
 sudo mv ./kubes-namespace-provisioner /usr/local/bin/kubes-namespace-provisioner
