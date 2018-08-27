2e9a502dd22b7021312eac4f2e82865d121441fa
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2018-08-27 13:16:13 -0600

    Update feature-ns-provisioner.sh

--- feature-ns-provisioner.sh
+++ feature-ns-provisioner.sh
@@ -10,8 +10,8 @@ echo "Installing namespace provisioner ${NAMESPACE_PROVISIONER_VERSION}"
 echo ""
 
 # Install the provisioning script
-AUTHORIZATION_HEADER=Authorization: token ${GITHUB_API_TOKEN}
-ACCEPT_HEADER=Accept: application/octet-stream
+AUTHORIZATION_HEADER="Authorization: token ${GITHUB_API_TOKEN}"
+ACCEPT_HEADER="Accept: application/octet-stream"
 
 NAMESPACE_PROVISIONER_RELEASE_URL=https://api.github.com/repos/commercialtribe/kubes-namespace-provisioner/releases/tags/${NAMESPACE_PROVISIONER_VERSION}
 NAMESPACE_PROVISIONER_DOWNLOAD_URL=`curl -sSL -H ${AUTHORIZATION_HEADER} ${NAMESPACE_PROVISIONER_RELEASE_URL} | jq -r '.assets[] | select(.name | contains("linux")).url'`
