e036f87b02202f719529b882ebfc2d5473276ead
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2018-09-17 11:33:55 -0600

    Update namespace provisioner version

--- feature-ns-provisioner.sh
+++ feature-ns-provisioner.sh
@@ -3,7 +3,7 @@ set -e
 # Install Psykube
 curl -sSL https://raw.githubusercontent.com/CommercialTribe/travis-scripts/master/psykube.sh | bash
 
-NAMESPACE_PROVISIONER_VERSION=${NAMESPACE_PROVISIONER_VERSION:-v1.2.3}
+NAMESPACE_PROVISIONER_VERSION=${NAMESPACE_PROVISIONER_VERSION:-v1.2.4}
 
 echo "#####################################################################"
 echo "# Installing namespace provisioner ${NAMESPACE_PROVISIONER_VERSION} #"
