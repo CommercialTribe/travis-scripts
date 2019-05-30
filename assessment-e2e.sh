5f4975fe8133eb9f8951e305f7fe113352bbf4fe
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2019-05-30 13:27:36 -0600

    Initial e2e script commands

--- assessment-e2e.sh
+++ assessment-e2e.sh
@@ -1,1 +1,29 @@
 set -e
+
+echo "Starting Assessment End-To-End Tests"
+echo "Images:"
+echo " assessment-e2e: ${IMAGE_E2E}"
+echo " assessment-frontend: ${IMAGE_ASSESSMENT_FRONTEND}"
+echo " assessment-api: ${IMAGE_ASSESSMENT_API}"
+
+# TODO Replace with latest image
+imageAssessmentApiLatest="${DOCKER_BASE_IMAGE_PATH}/assessment-api:feature-e2e"
+
+authHeader=`Authorization: token ${GITHUB_API_TOKEN}`
+acceptHeader='Accept: application/vnd.github.v3.raw'
+githubUrlPrefix=https://api.github.com/repos/CommercialTribe
+dockerComposeFile=docker-compose-e2e.yml
+
+echo "Getting ${dockerComposeFile}"
+dockerComposeE2e="${githubUrlPrefix}/assessment-e2e/contents/docker-compose-e2e.yml"
+curl -h ${authHeader} -h ${acceptHeader} -l ${dockerComposeE2e} > ${dockerComposeFile}
+
+echo "Getting assessment-api .env.docker"
+assessmentApiEnv="${githubUrlPrefix}/assessment-api/contents/.env.docker"
+curl -h ${authHeader} -h ${acceptHeader} -l ${assessmentApiEnv} > assessnent-api.env
+
+echo "Bootstrapping db"
+IMAGE_ASSESSMENT_API=${IMAGE_ASSESSMENT_API} docker-compose -f ${dockerComposeFile} run assessment-api yarn db:bootstrap
+
+echo "Running e2e tests"
+IMAGE_E2E=${IMAGE_E2E} IMAGE_ASSESSMENT_FRONTEND=${IMAGE_ASSESSMENT_FRONTEND} IMAGE_ASSESSMENT_API=${IMAGE_ASSESSMENT_API} docker-compose -f ${dockerComposeFile} up e2e
