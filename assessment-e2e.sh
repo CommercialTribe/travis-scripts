4cbca8c940430afa7ffc74147080adc1c043465f
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2019-12-16 15:12:01 -0700

    Write api logs on failure of e2e's

--- assessment-e2e.sh
+++ assessment-e2e.sh
@@ -7,13 +7,17 @@ echo " assessment-e2e: ${IMAGE_E2E}"
 echo " assessment-frontend: ${IMAGE_ASSESSMENT_FRONTEND}"
 echo " assessment-api: ${IMAGE_ASSESSMENT_API}"
 
+# Prep variables
 imageAssessmentApiLatest="${DOCKER_BASE_IMAGE_PATH}/assessment-api:latest"
+assessmentApiContainerName=e2e-assessment-api
+e2eLogsDirPath=e2e/logs
 
 authHeader="Authorization: token ${GITHUB_API_TOKEN}"
 acceptHeader='Accept: application/vnd.github.v3.raw'
 githubUrlPrefix=https://api.github.com/repos/CommercialTribe
 dockerComposeFile=docker-compose-e2e.yml
 
+# Download needed files
 echo "Getting ${dockerComposeFile}"
 dockerComposeE2e="${githubUrlPrefix}/assessment-e2e/contents/${dockerComposeFile}"
 curl -H "${authHeader}" -H "${acceptHeader}" -sSL ${dockerComposeE2e} > ${dockerComposeFile}
@@ -22,10 +26,20 @@ echo "Getting assessment-api .env.docker"
 assessmentApiEnv="${githubUrlPrefix}/assessment-api/contents/.env.docker"
 curl -H "${authHeader}" -H "${acceptHeader}" -sSL ${assessmentApiEnv} > assessment-api.env
 
-# We have to one line this section due to some weirdness with 
-# docker-compose run exiting this script and skipping all commands below it.
-echo "Bootstrapping db" && \
-IMAGE_ASSESSMENT_API="${imageAssessmentApiLatest}" docker-compose -f ${dockerComposeFile} run assessment-api yarn db:bootstrap && \
-echo "Running e2e tests" && \
-docker-compose -f ${dockerComposeFile} run e2e && \
+# Init and run tests
+echo "Bootstrapping db"
+IMAGE_ASSESSMENT_API="${imageAssessmentApiLatest}" docker-compose -f ${dockerComposeFile} run assessment-api yarn db:bootstrap
+echo "Running e2e tests"
+set +e
+docker-compose -f ${dockerComposeFile} run e2e
+if [ $? -ne 0 ]
+then
+  set -e
+  echo "e2e tests failed. Writing assessment-api logs for upload to S3"
+  mkdir -p ${e2eLogsDirPath}
+  docker logs ${assessmentApiContainerName} > "${assessmentApiContainerName}.txt"
+  exit $?
+fi
+
+set -e
 echo "Assessment End-To-End Tests Done"
