bd6ff58c87092f16e283cfa63dff5dcdcdc83633
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2019-12-16 19:29:32 -0700

    Use unbuffer

--- assessment-e2e.sh
+++ assessment-e2e.sh
@@ -28,7 +28,11 @@ curl -H "${authHeader}" -H "${acceptHeader}" -sSL ${assessmentApiEnv} > assessme
 
 # Init and run tests
 echo "Bootstrapping db"
-IMAGE_ASSESSMENT_API="${imageAssessmentApiLatest}" docker-compose -f ${dockerComposeFile} run -T assessment-api yarn db:bootstrap
+# FIXME Workaround for https://github.com/docker/compose/issues/7097
+sudo apt-get update
+sudo apt-get install expect-dev
+IMAGE_ASSESSMENT_API="${imageAssessmentApiLatest}" unbuffer docker-compose -f ${dockerComposeFile} run assessment-api yarn db:bootstrap
+
 echo "Running e2e tests"
 set +e
 docker-compose -f ${dockerComposeFile} run e2e
