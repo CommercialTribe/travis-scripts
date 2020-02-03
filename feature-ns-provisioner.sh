887e6360d0910473e03ec6aa1ccc20debbb810f3
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2020-02-03 16:49:35 -0700

    Update feature-ns-provisioner.sh

--- feature-ns-provisioner.sh
+++ feature-ns-provisioner.sh
@@ -16,6 +16,6 @@ ACCEPT_HEADER="Accept: application/octet-stream"
 NAMESPACE_PROVISIONER_RELEASE_URL=https://api.github.com/repos/commercialtribe/kubes-namespace-provisioner/releases/tags/${NAMESPACE_PROVISIONER_VERSION}
 NAMESPACE_PROVISIONER_DOWNLOAD_URL=`curl -sSL -H "${AUTHORIZATION_HEADER}" ${NAMESPACE_PROVISIONER_RELEASE_URL} | jq -r '.assets[] | select(.name | contains("linux")).url'`
 
-curl -L -H "${ACCEPT_HEADER}" -o kubes-namespace-provisioner ${NAMESPACE_PROVISIONER_DOWNLOAD_URL}?access_token=${GITHUB_API_TOKEN}
+curl -L -H "${ACCEPT_HEADER}" -H "${AUTHORIZATION_HEADER}" -o kubes-namespace-provisioner ${NAMESPACE_PROVISIONER_DOWNLOAD_URL}
 chmod +x ./kubes-namespace-provisioner
 sudo mv ./kubes-namespace-provisioner /usr/local/bin/kubes-namespace-provisioner
