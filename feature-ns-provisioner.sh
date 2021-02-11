643f6fd94076bd66d0bd7cfd9a3e196931c770f3
Jake Caban-Tomski <jacque006@users.noreply.github.com>
2021-02-10 19:14:58 -0700

    Rename, remove psykube

--- feature-ns-provisioner.sh
+++ feature-ns-provisioner.sh
@@ -1,7 +1,7 @@
 set -e
 
-# Install Psykube
-curl -sSL https://raw.githubusercontent.com/CommercialTribe/travis-scripts/master/psykube.sh | bash
+# Install gcloud
+curl -sSL https://raw.githubusercontent.com/CommercialTribe/travis-scripts/master/gcloud.sh | bash
 
 NAMESPACE_PROVISIONER_VERSION=${NAMESPACE_PROVISIONER_VERSION:-v4.0.2}
 
@@ -13,9 +13,9 @@ echo "#####################################################################"
 AUTHORIZATION_HEADER="Authorization: token ${GITHUB_API_TOKEN}"
 ACCEPT_HEADER="Accept: application/octet-stream"
 
-NAMESPACE_PROVISIONER_RELEASE_URL=https://api.github.com/repos/commercialtribe/kubes-namespace-provisioner/releases/tags/${NAMESPACE_PROVISIONER_VERSION}
+NAMESPACE_PROVISIONER_RELEASE_URL=https://api.github.com/repos/commercialtribe/kube-namespace-provisioner/releases/tags/${NAMESPACE_PROVISIONER_VERSION}
 NAMESPACE_PROVISIONER_DOWNLOAD_URL=`curl -sSL -H "${AUTHORIZATION_HEADER}" ${NAMESPACE_PROVISIONER_RELEASE_URL} | jq -r '.assets[] | select(.name | contains("linux")).url'`
 
-curl -L -H "${ACCEPT_HEADER}" -H "${AUTHORIZATION_HEADER}" -o kubes-namespace-provisioner ${NAMESPACE_PROVISIONER_DOWNLOAD_URL}
-chmod +x ./kubes-namespace-provisioner
-sudo mv ./kubes-namespace-provisioner /usr/local/bin/kubes-namespace-provisioner
+curl -L -H "${ACCEPT_HEADER}" -H "${AUTHORIZATION_HEADER}" -o kube-namespace-provisioner ${NAMESPACE_PROVISIONER_DOWNLOAD_URL}
+chmod +x ./kube-namespace-provisioner
+sudo mv ./kube-namespace-provisioner /usr/local/bin/kube-namespace-provisioner
